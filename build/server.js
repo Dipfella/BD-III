e131bc8734bbd1b1102cd68b3e8090a9233d7e65
Julian Sarmiento <julian.sarmiento.jeso.com>
2024-02-26 20:17:01 -0500

    Add post calc

--- build/server.js
+++ build/server.js
@@ -16,19 +16,32 @@ class Server {
         this.routes();
     }
     config() {
-        this.app.set('port', process.env.PORT || 4300);
+        this.app.set("port", process.env.PORT || 4300);
         //Middlewares
         this.app.use((0, morgan_1.default)("dev"));
         this.app.use((0, helmet_1.default)());
         this.app.use((0, compression_1.default)());
+        this.app.use(express_1.default.json());
         this.app.use((0, cors_1.default)());
     }
     routes() {
-        this.app.get("/api", (req, res) => res.send('Hola Mundo'));
+        this.app.get("/api", (req, res) => res.send("Hola Mundo"));
+        //todas las operaciones basicas
+        this.app.post("/api/calc", (req, res) => {
+            console.log(req.body);
+            let num1 = req.body.numA;
+            let num2 = req.body.numB;
+            res.json({
+                Suma: num1 + num2,
+                Resta: num1 - num2,
+                Multiplicacion: num1 * num2,
+                Division: num1 / num2
+            });
+        });
     }
     Start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log('server funcionando...' + this.app.get('port'));
+        this.app.listen(this.app.get("port"), () => {
+            console.log("server funcionando..." + this.app.get("port"));
         });
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,49 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import compression from 'compression';
-import cors from 'cors'
-
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
 
 class Server {
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor() {
-        this.app = express(); // mismo nombre de la clase
-        this.config();
-        this.routes();
-    }
+  constructor() {
+    this.app = express(); // mismo nombre de la clase
+    this.config();
+    this.routes();
+  }
 
-    config() {
-        this.app.set('port', process.env.PORT || 4300);
-        //Middlewares
-        this.app.use(morgan("dev"));
-        this.app.use(helmet())
-        this.app.use(compression());
-        this.app.use(cors());
-    }
+  config() {
+    this.app.set("port", process.env.PORT || 4300);
+    //Middlewares
+    this.app.use(morgan("dev"));
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
 
-    routes () {
-        this.app.get("/api",(req,res) => res.send('Hola Mundo'));
-    }
+  routes() {
+    this.app.get("/api", (req, res) => res.send("Hola Mundo"));
+    //todas las operaciones basicas
+    this.app.post("/api/calc", (req, res) => {
+        console.log(req.body)
+      let num1: number = req.body.numA;
+      let num2: number = req.body.numB;
+      res.json({
+        Suma: num1 + num2,
+        Resta: num1 - num2,
+        Multiplicacion: num1 * num2,
+        Division: num1 / num2
+      });
+    });
+  }
 
-    public Start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log('server funcionando...' + this.app.get('port'))
-        });
-    }
+  public Start() {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("server funcionando..." + this.app.get("port"));
+    });
+  }
 }
 
 export { Server };
